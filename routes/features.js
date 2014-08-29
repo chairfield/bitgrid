6a76d4f21c07b2dccae9ced4525eaf01b867201c
Chris Hairfield <chairfield@gmail.com>
2014-08-29 13:55:02 -0700

    Both cleaned and tightened up the code by including some tiny helper functions and ensuring that I don't commit to mongo if an error condition exists.

--- routes/features.js
+++ routes/features.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+String.prototype.isEmpty = function() {
+    return (this.length === 0 || !this.trim());
+};
+
+function isLat(lat) {
+    if (isNaN(lat)) return false;
+    var asNumber = Number(lat);
+    return asNumber < 90 && asNumber > -90;
+}
+
+function isLon(lon) {
+    if (isNaN(lon)) return false;
+    var asNumber = Number(lon);
+    return asNumber < 180 && asNumber > -180;
+}
+
 router.route('/features')
     .all(function(req, res, next) {
         // This is fantastic; monk seems to cache the connection as every time this runs after the first
         // is an order of magnitude less than the first time (~200 ms to ~20 ms).
         req.features = require('monk')('mongodb://@localhost:27017/test').get('features');
+        console.log(req.body);
         next();
     })
     .get(function(req, res, next) {
@@ -19,12 +36,18 @@ router.route('/features')
         // This is more to be helpful to users of a REST client like Postman who forget to set Content-Type.
         if (req.headers["content-type"].toLowerCase() != "application/json") {
             res.send("Invalid Content-Type: must be application/json");
+            return;
         }
 
-        if (req.body.name == null ||
-            req.body.lat == null ||
-            req.body.lon == null) {
-            res.send("Invalid post: object must contain name, lat, and lon properties.");
+        var name = req.body.name;
+        if (!name.isEmpty || name.isEmpty()) {
+            res.send("Invalid name: must be a non-blank string.");
+            return;
+        }
+
+        if (!isLat(req.body.lat) || !isLon(req.body.lon)) {
+            res.send("Invalid lat/lon: latitude must be within -90 and 90 while longitude must be within -180 and 180.");
+            return;
         }
 
         console.log("inserting feature", req.body.name, req.body.lat, req.body.lon);
