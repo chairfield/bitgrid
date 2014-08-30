3bfbe9e84289b70e65ce79db293ac2f9447e9f64
Chris Hairfield <chairfield@gmail.com>
2014-08-30 09:15:28 -0700

    The home page now has a leaflet map. I'm using browserify to package all my client-side javascript into a single bundle.js exposed under /public/javascripts.

---
+++ client/src/client.js
@@ -0,0 +1,11 @@
+var L = require('leaflet');
+
+L.Icon.Default.imagePath = 'http://leafletjs.com/dist/images';
+
+window.onload = function() {
+    var div = document.body.appendChild(document.createElement('div'));
+    div.style.cssText = 'height:500px;';
+    var map = L.map(div).setView([0, 0], 2);
+    L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    var gj = L.geoJson().addTo(map);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var browserify = require('browserify');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index');
+});
+
+/* Host the bundle.js file that the home page references. */
+router.get('/javascripts/bundle.js', function(req, res) {
+    var b = browserify();
+    b.add('./client/src/client.js');
+    b.bundle().pipe(res);
 });
 
 module.exports = router;
