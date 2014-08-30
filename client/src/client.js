231b136703c18d18b3d69d2130d6cf3c7fead333
Chris Hairfield <chairfield@gmail.com>
2014-08-30 10:28:27 -0700

    Made the leaflet map fullscreen.

--- client/src/client.js
+++ client/src/client.js
@@ -4,7 +4,7 @@ L.Icon.Default.imagePath = 'http://leafletjs.com/dist/images';
 
 window.onload = function() {
     var div = document.body.appendChild(document.createElement('div'));
-    div.style.cssText = 'height:500px;';
+    div.id = 'map';
     var map = L.map(div).setView([0, 0], 2);
     L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
     var gj = L.geoJson().addTo(map);
