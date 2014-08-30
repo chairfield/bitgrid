29d44b9004736bd94976d828c08f49e9d6a4665a
Chris Hairfield <chairfield@gmail.com>
2014-08-30 10:34:38 -0700

    Centered the map on DC.

--- client/src/client.js
+++ client/src/client.js
@@ -5,7 +5,10 @@ L.Icon.Default.imagePath = 'http://leafletjs.com/dist/images';
 window.onload = function() {
     var div = document.body.appendChild(document.createElement('div'));
     div.id = 'map';
-    var map = L.map(div).setView([0, 0], 2);
+
+    var DC_COORDS = [38.914268, -77.021098];
+    var map = L.map(div).setView(DC_COORDS, 13);
+
     L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
     var gj = L.geoJson().addTo(map);
 };
